b82d6dceec23cbb60da9da8c218a5847a559e9ca
Bhargav-Kulkarni84 <coderbhargav8237@gmail.com>
2025-08-03 22:15:20 +0530

    Added React Router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,22 @@
+import { BrowserRouter } from 'react-router-dom'
+import {Routes,Route} from 'react-router-dom'
 
 function App() {
 
   return (
-    <div>
-        Hello world
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+
+          <Route path="/signup"> element={<Singup/>}</Route>
+          <Route path="/signin"> element={<Singin/>}</Route>
+          <Route path="/dashboard"> element={<Dashboard/>}</Route>
+          <Route path="/send"> element={<SendMoney/>}</Route>
+
+        </Routes>
+      </BrowserRouter>
+    </>
+
   )
 }
 
